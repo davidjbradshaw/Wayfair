a99a8e00da8b0d0d3de13e8ad5602c1934383c09
David J. Bradshaw <dave@bradshaw.net>
2021-02-10 13:43:19 +0000

    tidy

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
 import ErrorBoundary from './components/error-boundary'
 
 ReactDOM.render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>,
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
