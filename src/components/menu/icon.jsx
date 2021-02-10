7f1a6d74b2d3493312ccf18cb794316fcaaeda3c
David J. Bradshaw <dave@bradshaw.net>
2021-02-10 12:45:14 +0000

    first pass

---
+++ src/components/menu/icon.jsx
@@ -0,0 +1,21 @@
+import Button from '@material-ui/core/Button'
+
+const iconStyle = {
+  position: 'absolute',
+  top: '14px',
+  right: '0',
+  color: '#7f187f',
+}
+
+const Menu = ({ onClick }) => (
+  <Button
+    aria-controls="simple-menu"
+    aria-haspopup="true"
+    onClick={onClick}
+    style={iconStyle}
+  >
+    <span class="material-icons md-24">account_circle</span>
+  </Button>
+)
+
+export default Menu
---
+++ src/components/menu/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Icon from './icon'
+
+const menuStyle = {
+  marginTop: '32px',
+}
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div>
+      <Icon onClick={handleClick} />
+      <Menu
+        id="simple-menu"
+        style={menuStyle}
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Account Settings</MenuItem>
+        <MenuItem onClick={handleClose}>User Management</MenuItem>
+        <MenuItem onClick={handleClose}>My Team</MenuItem>
+        <MenuItem onClick={handleClose}>English (UK)</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  )
+}
